a14690963500a1e74ba72ca4106e4b2f7ee6f74f
emfo2405 <emfo2405@student.miun.se>
2025-03-03 08:38:32 +0100

    kommentarer tillagda för funktioner i javascript

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,125 +1,140 @@
-  "use strict";
-  
-  //En tom array
-  let data = [];
+"use strict";
 
+//En tom array för att fylla med data för API
+let data = [];
+
+//Kör funktionen fetchData vid inladdning av sidan
 window.onload = fetchData;
 
-  //Hämta in data för kurser och program
-  async function fetchData() {
-    try {
-        //Hämtar in data
-        const response = await fetch('https://studenter.miun.se/~mallar/dt211g/');
-        //Felmeddelande om datan inte läses in korrekt
-        if (!response.ok) {
-            throw new Error("Fel vid anslutning till data...");
-        }
-
-        //sparar datan till den tomma arrayen
-        data = await response.json();
-
-        //Kör funktioner för att skriva ut data
-
-        sortAppl(data);
-
-        //Felmeddelande om något går fel
-    } catch (error) {
-        console.error(error);
+//Hämta in data för kurser och program
+/**
+ * Funktion för att hämta in data om kurser och program
+ * @function fetchData
+ * @param response - Hämtar in data om kurser
+ * @param data - Den inhämtade datan
+ * Sedan körs funktionen sortAppl med data som värde
+ */
+async function fetchData() {
+  try {
+    //Hämtar in data
+    const response = await fetch('https://studenter.miun.se/~mallar/dt211g/');
+    //Felmeddelande om datan inte läses in korrekt
+    if (!response.ok) {
+      throw new Error("Fel vid anslutning till data...");
     }
+
+    //sparar datan till den tomma arrayen
+    data = await response.json();
+
+    //Kör funktioner för att skriva ut data
+
+    sortAppl(data);
+
+    //Felmeddelande om något går fel
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 //Sortera datan efter mest sökta (både program och kurser)
+/**
+ * @function sortAppl - Funktion som sorterar data 
+ * @param data - den data som hämtats för kurser och program 
+ * data.sort sorterar den inmatade datan efter totalt antal sökande från mest till minst
+ * Funktionerna printCourses och printPrograms körs med sorterad data som inmatning
+ */
 function sortAppl(data) {
-  
-    data.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
+
+  data.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
 
 
-    printCourses(data);
-    printPrograms(data);
+  printCourses(data);
+  printPrograms(data);
 
 }
 
 
 //Funktion för att ta ut de 6 mest sökta kurserna och deras namn samt antal sökande
+/**
+ * @function printCourses - funktion som tar ut topp 6 mest sökta kurser och tar ut antal sökande och namn för dem
+ * @param data - en sorterad lista av program och kurser baserat på flest antal sökande
+ * @param {Array} courseEl - En array som fylls med data för typen kurser
+ * @param {Array} top6 - en array som tar kurser med index 0 till 6 med flest antal sökande
+ * @param {Array} cName - en array med namn på kurserna i top6
+ * @param {Array} appl - en array med totalt antal sökande i top6
+ * Sedan körs funktionen drawDiagram
+ */
 function printCourses(data) {
   let courseEl = [];
   courseEl = data.filter(val => val.type == "Kurs");
 
 
 
-  const top5 = courseEl.slice(0,6);
+  const top6 = courseEl.slice(0, 6);
 
-  const cName = top5.map(kurs => kurs.name);
-  const appl = top5.map(kurs => kurs.applicantsTotal);
+  const cName = top6.map(kurs => kurs.name);
+  const appl = top6.map(kurs => kurs.applicantsTotal);
 
 
 
-drawDiagram(cName, appl)
+  drawDiagram(cName, appl)
 
 }
 
 //Funktion för att ta ut de 5 mest sökta programmen och deras namn samt antal sökande
+/**
+ * @function printPrograms - funktion som tar ut topp 6 mest sökta program och tar ut antal sökande och namn för dem
+ * @param data - en sorterad lista av program och kurser baserat på flest antal sökande
+ * @param {Array} programEl - En array som fylls med data för typen program
+ * @param {Array} top5 - en array som tar program med index 0 till 5 med flest antal sökande
+ * @param {Array} pName - en array med namn på program i top5
+ * @param {Array} pappl - en array med totalt antal sökande i top5
+ * Sedan körs funktionen drawDiagram
+ */
+
 function printPrograms(data) {
   let programEl = [];
   programEl = data.filter(val => val.type == "Program");
 
 
 
-  const top5 = programEl.slice(0,5);
+  const top5 = programEl.slice(0, 5);
 
   const pName = top5.map(program => program.name);
   const pAppl = top5.map(program => program.applicantsTotal);
 
 
-drawDiagramP(pName, pAppl)
+  drawDiagramP(pName, pAppl)
 
 }
 
 
 
 //Diagram över sökta kurser
+/**
+ * @function drawDiagram - ritar ett stapeldiagram
+ * @param {Array} cName - namn över kurser 
+ * @param {Array} appl - totalt antal sökande i kurserna
+ * @param ctx - hämtar ett element med ID 'myChart'
+ * @param Chart.defaults.backgroundColor - Ändrar defaultfärg för bakgrund
+ * @param Chart.defaults.borderColor - Ändrar defaultfärg på border
+ * @param chart - skapar ett stapeldiagram över data cName och appl 
+ */
 
 function drawDiagram(cName, appl) {
-const ctx = document.getElementById('myChart');
+  const ctx = document.getElementById('myChart');
 
-Chart.defaults.backgroundColor = '#861f0d';
-Chart.defaults.borderColor = '#6f1a0b';
+  Chart.defaults.backgroundColor = '#861f0d';
+  Chart.defaults.borderColor = '#6f1a0b';
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: cName,
-    datasets: [{
-      label: 'Totalt antal sökande',
-      data: appl,
-      borderWidth: 2
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-    
-  }
-});
-
-}
-
-//Rita diagram över program
-function drawDiagramP(pName, pAppl) {
-  const ctx = document.getElementById('myChart2');
-  
   new Chart(ctx, {
-    type: 'pie',
+    type: 'bar',
     data: {
-      labels: pName,
+      labels: cName,
       datasets: [{
         label: 'Totalt antal sökande',
-        data: pAppl,
-        borderWidth: 2,
-        backgroundColor: ['#861f0d','#56423D','#BEA6A0','#541308','#110402']
+        data: appl,
+        borderWidth: 2
       }]
     },
     options: {
@@ -128,9 +143,44 @@ function drawDiagramP(pName, pAppl) {
           beginAtZero: true
         }
       }
-      
+
     }
   });
-  
-  }
+
+}
+
+//Rita diagram över program
+/**
+ * @function drawDiagramP - ritar ett stapeldiagram
+ * @param {Array} pName - namn över program 
+ * @param {Array} pAppl - totalt antal sökande i programmen
+ * @param ctx - hämtar ett element med ID 'myChart2'
+ * @param chart - skapar ett cirkeldiagram över data pName och pAppl
+ * @param backgroundColor - Ändrar färger på delarna i cirkeldiagram 
+ */
+function drawDiagramP(pName, pAppl) {
+  const ctx = document.getElementById('myChart2');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: pName,
+      datasets: [{
+        label: 'Totalt antal sökande',
+        data: pAppl,
+        borderWidth: 2,
+        backgroundColor: ['#861f0d', '#56423D', '#BEA6A0', '#541308', '#110402']
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+
+    }
+  });
+
+}
 
--- src/js/karta.js
+++ src/js/karta.js
@@ -1,17 +1,20 @@
 "use strict";
 
+//Skapa en tom array för att lagra inläst data i
 let data = [];
 
-
-
-
-let map = document.getElementById("karta");
-
-
-
-
+//När man klickar på sökknappen körs funktionen fetchData för att hämta in data för sökningen
 document.getElementById('search-button').onclick = fetchData;
 
+//Funktion för att hämta in data från API
+/**
+ * Hämtar in data från nominatim API för en plats
+ * @function fetchData
+ * @param {value} searchInput - Tar in värdet för det som skrivs in i sökrutan
+ * @param response - Hämtar API för nominatim
+ * @param data - Svar från inhämtat API
+ * Sedan körs funktionen takeData med @param data som input
+ */
 async function fetchData() {
     try {
         //Hämtar in data
@@ -39,6 +42,22 @@ async function fetchData() {
     }
 }
 
+//Funktion för att ta ut longitud och latitud för sökt plats
+/**
+ * @function takeData
+ * @param {*} data - Hämtat API från fetchData
+ * @param {Array} latitudeSearch - Tar ut latituder för de platser som matchar det som skrivits in i sökrutan
+ * @param {Number} latitudeSearchEl - Tar ut latitud för den första platsen i arrayen latitudeSearch
+ * @param {Array} longitudeSearch - Tar ut longituder för de platser som matchar det som skrivits in i sökrutan
+ * @param {Number} longitudeSearchEl - Tar ut longitud för den första platsen i arrayen i longitudeSearch
+ * Värden för att skapa en boundingbox behövs för att zooma in på den plats på kartan man vill visa
+ * Detta skapas här genom att ta värden för min latitud, min longitud, max longitud och max latitud
+ * @param {Number} minLat - Latitud från latitudeSearchEl - 0,1 för att få min longitud
+ * @param {Number} maxLat - Latitud från latitudeSearchEl + 0,1 för att få max latitud
+ * @param {Number} minLong - Longitud från longitudeSearchEl - 0.1 för att få min longitud
+ * @param {Number} maxLong - Longitud från longitudeSearchEl + 0.1 för att få max longitud
+ * Till sist uppdateras länken för kartan med variabler för latitud och longitud för att visa kartan för vald plats
+ */
 function takeData(data) {
 
     const latitudeSearch = data.map(plats => plats.lat);
